f4d3e98c700cbe4d44cc25f94e7e50e441a4de75
Igor Katselenbogen <orange-juice@Igors-MacBook-Pro.local>
2019-09-08 15:12:41 +0300

    Configuration to deploy on heroku

--- services/EventService.js
+++ services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://my-json-server.typicode.com/underwoodkats/real-world-nuxt/',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
