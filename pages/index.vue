de036b842224d71cfceb4b60373b286ed633399e
Igor Katselenbogen <orange-juice@Igors-MacBook-Pro.local>
2019-09-08 14:35:06 +0300

    Lesson 8 - Adding Vuex

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,37 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import EventCard from '../components/EventCard'
+
 export default {
+  components: { EventCard },
   head() {
     return {
       title: 'Event Listing'
+    }
+  },
+  computed: mapState({
+    events: (state) => state.events.events
+  }),
+  async fetch({ store, error }) {
+    try {
+      await store.dispatch('events/fetchEvents')
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch data!'
+      })
     }
   }
 }
